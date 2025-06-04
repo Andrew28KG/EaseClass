e017ec7d2433ab349c019de8f5eae8ca934cf549
Endru <117648461+Andrew28KG@users.noreply.github.com>
2025-06-04 14:15:51 +0700

    final

--- easeclass/lib/pages/admin/class_management_help_page.dart
+++ easeclass/lib/pages/admin/class_management_help_page.dart
@@ -88,4 +88,4 @@ class ClassManagementHelpPage extends StatelessWidget {
       ),
     );
   }
-} 
+}
--- easeclass/lib/pages/admin/user_management_help_page.dart
+++ easeclass/lib/pages/admin/user_management_help_page.dart
@@ -97,4 +97,4 @@ class UserManagementHelpPage extends StatelessWidget {
       ),
     );
   }
-} 
+}
